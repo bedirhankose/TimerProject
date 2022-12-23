dca9ae65670745164e2bce0c9e894d20334591af
Bedirhan Köse <bedirhan.kose05@gmail.com>
2022-12-23 16:37:32 +0100

    Timer
    A simple countdown app.

--- TimerProject/ViewController.swift
+++ TimerProject/ViewController.swift
@@ -9,11 +9,36 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var TimerLabel: UILabel!
+    
+    var timer = Timer()
+    var kalanZaman = 0
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        kalanZaman = 15
+        
+        TimerLabel.text = "Zaman: \(kalanZaman)"
+
     }
 
-
+    @IBAction func TimeStarted(_ sender: Any) {
+        
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(timerFunction), userInfo: nil, repeats: true)
+    }
+    
+    @objc func timerFunction() {
+        TimerLabel.text = "Zaman: \(kalanZaman)"
+        
+        kalanZaman = kalanZaman - 1
+        
+        if kalanZaman == 0 {
+            TimerLabel.text = " Sure bitti..."
+            timer.invalidate()
+            kalanZaman = 15
+        }
+    }
 }
 
